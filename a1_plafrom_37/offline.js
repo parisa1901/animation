d861042fb098ea8ee6d6d33cb893d7dae8978350
parisa1901 <s43499@bj.ac.th>
2025-11-19 10:58:40 +0700

    Add files via upload

---
+++ a1_plafrom_37/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1763524668,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
